aeb47934ea708077928147a7a483f6af6cd38c71
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2022-03-20 17:01:15 -0400

    Fix creation of Finally, update sp4 json
    Fix how we generate Finally from another predicate by passing a timed predicate instead of an untimed predicate.
    There's still some funniness to sort out with predicates and annotations.
    The modular predicates we'd like to use should all be "Finally(p, t)" for some convergence time t.
    The annotations will vary more but essentially be either "Finally(p, t1)" or "Until(t1, p, q)" for some t1 <= t.

--- Gardener/AstFunction/Finally.cs
+++ Gardener/AstFunction/Finally.cs
@@ -8,22 +8,18 @@ public class Finally<T> : AstPredicate<Pair<T, BigInteger>>
 {
   public BigInteger Time { get; set; }
 
-  public Finally(string arg, BigInteger time, AstPredicate<T> pred) : base(arg, Wrap(arg, pred, time))
+  public Finally(BigInteger time, AstPredicate<Pair<T, BigInteger>> predicate) : base(predicate.Arg, Wrap(predicate, time))
   {
     Time = time;
   }
 
-  private static Expr<bool, Pair<T, BigInteger>> Wrap(string arg, AstPredicate<T> pred, BigInteger time)
+  private static Expr<bool, Pair<T, BigInteger>> Wrap(AstPredicate<Pair<T, BigInteger>> predicate, BigInteger time)
   {
-    var a = new Var<Pair<T, BigInteger>>(arg);
+    var a = new Var<Pair<T, BigInteger>>(predicate.Arg);
     var t = new ConstantExpr<BigInteger, Pair<T, BigInteger>>(time);
     // true if the second element of a < time
     var beforeBranch =
       new LessThan<BigInteger, Pair<T, BigInteger>>(new Second<T, BigInteger, Pair<T, BigInteger>>(a), t);
-    // true if the predicate holds for the first element of a
-    // construct a statement saying either the second element t < time or the expr holds for the second element
-    // return new Or<Pair<T, BigInteger>>(new LessThan<T, Pair<T, BigInteger>>(
-    // new Second<T, BigInteger, Pair<T, BigInteger>>(a), new ConstantExpr<T, Pair<T, BigInteger>>(time)));
-    throw new NotImplementedException();
+    return new Or<Pair<T, BigInteger>>(beforeBranch, predicate.Body);
   }
 }
