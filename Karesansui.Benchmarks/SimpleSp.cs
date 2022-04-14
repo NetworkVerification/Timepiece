379a6fd78f64d005f2421ce685606692e22d0239
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2022-04-14 13:06:21 -0400

    Add an alternative route type SimpleBgpRoute to BatfishBgpRoute
    At this point the only difference between the two is SBR doesn't have a Communities field like BBR.

---
+++ Karesansui.Benchmarks/SimpleSp.cs
@@ -0,0 +1,79 @@
+using System.Numerics;
+using ZenLib;
+
+namespace Karesansui.Benchmarks;
+
+public class SimpleSp<TS> : FatTree<Option<SimpleBgpRoute>, TS>
+{
+  public SimpleSp(Topology topology, string destination,
+    Dictionary<string, Func<Zen<Option<SimpleBgpRoute>>, Zen<BigInteger>, Zen<bool>>> annotations,
+    IReadOnlyDictionary<string, Func<Zen<Option<SimpleBgpRoute>>, Zen<bool>>> stableProperties,
+    IReadOnlyDictionary<string, Func<Zen<Option<SimpleBgpRoute>>, Zen<bool>>> safetyProperties,
+    SymbolicValue<TS>[] symbolics) :
+    base(topology, destination,
+      topology.ForAllEdges(_ => Lang.Omap<SimpleBgpRoute, SimpleBgpRoute>(SimpleBgpRouteExtensions.IncrementAsPath)),
+      Lang.Omap2<SimpleBgpRoute>(SimpleBgpRouteExtensions.Min),
+      Option.Create<SimpleBgpRoute>(new SimpleBgpRoute()), Option.None<SimpleBgpRoute>(),
+      annotations, stableProperties, safetyProperties, symbolics)
+  {
+  }
+}
+
+/// <summary>
+/// Static factory class for Sp networks.
+/// </summary>
+public static class SimpleSp
+{
+  public static SimpleSp<Unit> Reachability(uint numPods, string destination)
+  {
+    var topology = Topologies.FatTree(numPods);
+    var distances = topology.BreadthFirstSearch(destination);
+    var reachable = Lang.IsSome<SimpleBgpRoute>();
+    var annotations =
+      distances.Select(p => (p.Key, Lang.Finally(p.Value, reachable)))
+        .ToDictionary(p => p.Item1, p => p.Item2);
+    var stableProperties = topology.ForAllNodes(_ => reachable);
+    // no safety property
+    var safetyProperties = topology.ForAllNodes(_ => Lang.True<Option<SimpleBgpRoute>>());
+    return new SimpleSp<Unit>(topology, destination, annotations, stableProperties, safetyProperties,
+      Array.Empty<SymbolicValue<Unit>>());
+  }
+
+  // slightly weaker path length property with simpler annotations
+  public static SimpleSp<Unit> PathLengthNoSafety(uint numPods, string destination)
+  {
+    var topology = Topologies.FatTree(numPods);
+    var distances = topology.BreadthFirstSearch(destination);
+
+    var annotations =
+      distances.Select(p => (p.Key, Lang.Until(p.Value,
+          Lang.OrSome<SimpleBgpRoute>(b => Zen.And(b.LpEquals(100), b.GetAsPathLength() >= BigInteger.Zero)),
+          Lang.IfSome(SimpleBgpRouteExtensions.MaxLengthDefaultLp(p.Value)))))
+        .ToDictionary(p => p.Item1, p => p.Item2);
+
+    var stableProperties =
+      topology.ForAllNodes(_ => Lang.IfSome<SimpleBgpRoute>(b => b.LengthAtMost(new BigInteger(4))));
+    var safetyProperties = topology.ForAllNodes(_ => Lang.True<Option<SimpleBgpRoute>>());
+    return new SimpleSp<Unit>(topology, destination, annotations, stableProperties, safetyProperties,
+      Array.Empty<SymbolicValue<Unit>>());
+  }
+
+  public static SimpleSp<Unit> PathLength(uint numPods, string destination)
+  {
+    var topology = Topologies.FatTree(numPods);
+    var distances = topology.BreadthFirstSearch(destination);
+
+    var annotations =
+      distances.Select(p => (p.Key, Lang.Until(p.Value,
+          Lang.IsNone<SimpleBgpRoute>(),
+          Lang.IfSome(SimpleBgpRouteExtensions.MaxLengthDefaultLp(p.Value)))))
+        .ToDictionary(p => p.Item1, p => p.Item2);
+
+    var stableProperties =
+      topology.ForAllNodes(_ => Lang.IfSome<SimpleBgpRoute>(b => b.LengthAtMost(new BigInteger(4))));
+    var safetyProperties = topology.ForAllNodes(_ =>
+      Lang.Union(Lang.IsNone<SimpleBgpRoute>(), Lang.IfSome<SimpleBgpRoute>(b => b.LengthAtMost(new BigInteger(4)))));
+    return new SimpleSp<Unit>(topology, destination, annotations, stableProperties, safetyProperties,
+      Array.Empty<SymbolicValue<Unit>>());
+  }
+}
