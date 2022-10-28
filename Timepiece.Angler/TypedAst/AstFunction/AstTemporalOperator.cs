4d14d4f8a32eeca179b23e2255f5f4e831f55fd6
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2022-10-28 13:09:35 -0400

    Delete out-of-date TypedAst code

--- Timepiece.Angler/TypedAst/AstFunction/AstTemporalOperator.cs
+++
@@ -1,9 +0,0 @@
-using System.Numerics;
-using ZenLib;
-
-namespace Timepiece.Angler.TypedAst.AstFunction;
-
-public abstract class AstTemporalOperator<T>
-{
-  public abstract Func<Zen<T>, Zen<BigInteger>, Zen<bool>> Evaluate(Func<string, AstPredicate<T>> getter);
-}
