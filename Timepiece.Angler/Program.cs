89901c44a7ac62f58d339cc0ba9f7a5661793e40
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2023-03-08 15:37:12 -0500

    Change Angler to use System.CommandLine
    Use CommandLine for simpler argument parsing

--- Timepiece.Angler/Program.cs
+++ Timepiece.Angler/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.CommandLine;
+using System.Diagnostics;
 using Newtonsoft.Json;
 using Timepiece;
 using Timepiece.Angler;
@@ -18,31 +19,57 @@ JsonSerializer Serializer()
   };
 }
 
-foreach (var file in args)
-{
-  var json = new JsonTextReader(new StreamReader(file));
-  RouteEnvironmentAst? ast;
-  var isInternet2 = false;
-  if (file.Contains("INTERNET2") || file.Contains("internet2") || file.Contains("BAGPIPE") || file.Contains("bagpipe"))
+var rootCommand = new RootCommand("Timepiece benchmark runner");
+var monoOption = new Option<bool>(
+  new[] {"--mono", "--ms", "-m"},
+  "If given, run the benchmark monolithically simulating Minesweeper");
+var validateOption = new Option<bool>(
+  new[] {"--validate", "-V"},
+  "If given, validate the benchmark before running."
+);
+var fileArgument = new Argument<string>(
+  name: "file",
+  description: "The .angler.json file to use");
+rootCommand.Add(fileArgument);
+rootCommand.Add(monoOption);
+rootCommand.Add(validateOption);
+rootCommand.SetHandler(
+  (file, mono, validate) =>
   {
-    Console.WriteLine("Internet2 benchmark identified...");
-    ast = Serializer().Deserialize<Internet2>(json);
-    isInternet2 = true;
-  }
-  else
-    ast = Serializer().Deserialize<RouteEnvironmentAst>(json);
+    var json = new JsonTextReader(new StreamReader(file));
+    RouteEnvironmentAst? ast;
+    var isInternet2 = false;
+    if (file.Contains("INTERNET2") || file.Contains("internet2") || file.Contains("BAGPIPE") ||
+        file.Contains("bagpipe"))
+    {
+      Console.WriteLine("Internet2 benchmark identified...");
+      ast = Serializer().Deserialize<Internet2>(json);
+      isInternet2 = true;
+    }
+    else
+      ast = Serializer().Deserialize<RouteEnvironmentAst>(json);
 
-  Console.WriteLine($"Successfully deserialized JSON file {file}");
-  Debug.WriteLine("Running in debug mode...");
-  Debug.WriteLine("Warning: additional assertions in debug mode may substantially slow running time!");
-  json.Close();
-  if (ast != null)
-  {
-    // ast.Validate();
-    Profile.RunCmpPerNode(isInternet2
-      ? ((Internet2) ast).ToNetwork(BlockToExternal.StrongInitialConstraints)
-      : ast.ToNetwork());
-    // Profile.RunAnnotatedWithStats(ast.ToNetwork());
-  }
-  else Console.WriteLine("Failed to deserialize contents of {file} (received null).");
-}
+    Console.WriteLine($"Successfully deserialized JSON file {file}");
+    Debug.WriteLine("Running in debug mode...");
+    Debug.WriteLine("Warning: additional assertions in debug mode may substantially slow running time!");
+    json.Close();
+    if (ast != null)
+    {
+      if (validate) ast.Validate();
+      if (mono)
+      {
+        Profile.RunMonoWithStats(isInternet2
+          ? ((Internet2) ast).ToNetwork(BlockToExternal.StrongInitialConstraints)
+          : ast.ToNetwork());
+      }
+      else
+      {
+        Profile.RunAnnotatedWithStats(isInternet2
+          ? ((Internet2) ast).ToNetwork(BlockToExternal.StrongInitialConstraints)
+          : ast.ToNetwork());
+      }
+    }
+    else Console.WriteLine("Failed to deserialize contents of {file} (received null).");
+  }, fileArgument, monoOption, validateOption);
+
+await rootCommand.InvokeAsync(args);
