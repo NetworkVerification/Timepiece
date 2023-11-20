165854c6740a066f40b4038f1051b38c26d1a16a
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2023-11-20 15:02:23 -0500

    Change AstFunction to record

--- Timepiece.Angler/Ast/AstFunction.cs
+++ Timepiece.Angler/Ast/AstFunction.cs
@@ -7,20 +7,13 @@ namespace Timepiece.Angler.Ast;
 ///   A unary function of type T to Option T.
 /// </summary>
 /// <typeparam name="T">The type of the function's input and output.</typeparam>
-public class AstFunction<T>
+public record AstFunction<T>(string Arg, IEnumerable<Statement> Body)
 {
-  [System.Text.Json.Serialization.JsonConstructor]
-  public AstFunction(string arg, IEnumerable<Statement> body)
-  {
-    Arg = arg;
-    Body = body;
-  }
+  [JsonProperty(Required = Required.Always)]
+  public string Arg { get; set; } = Arg;
 
   [JsonProperty(Required = Required.Always)]
-  public string Arg { get; set; }
-
-  [JsonProperty(Required = Required.Always)]
-  public IEnumerable<Statement> Body { get; set; }
+  public IEnumerable<Statement> Body { get; set; } = Body;
 
   public void Rename(string oldArg, string newArg)
   {
