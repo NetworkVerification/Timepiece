e79cc946db82e80bbc5a34222aeec64e5a95303e
Tim Alberdingk Thijm <tthijm@della8.princeton.edu>
2023-12-12 11:51:10 -0500

    Change make_dat to specify the type of benchmark

--- make_dat.py
+++ make_dat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Print a .dat to stdout using the given benchmark log file.
-# Usage: make_dat.py [log file]
+# Usage: make_dat.py [log file] [mono|modular]
 
 import csv
 import itertools
@@ -26,29 +26,40 @@ def output_to_rows(s: str, is_mono: bool = False) -> list[dict[str, float]]:
     ]
 
 
+if len(sys.argv) < 2:
+    raise Exception("Usage: make_dat.py [log file] [mono|modular]")
+
 log_file = pathlib.PurePath(sys.argv[1])
-is_mono = "mono" in str(log_file)
+if sys.argv[2] == "mono":
+    is_mono = True
+elif sys.argv[2] == "modular":
+    is_mono = False
+else:
+    raise Exception("Must specify modular or mono!")
+
 with open(log_file, "r") as log:
     rows = output_to_rows(log.read(), is_mono)
 if is_mono:
-    headers = ["n", "total"]
+    headers = {"n": int, "total": float}
 else:
-    headers = [
-        "n",
-        "max",
-        "min",
-        "avg",
-        "med",
-        "99p",
-        "total",
-        "wall",
-    ]
+    headers = {
+            "n": int,
+            "max": float,
+            "min": float,
+            "avg": float,
+            "med": float,
+            "99p": float,
+            "total": float,
+            "wall": float,
+    }
 min_rows = []
 for _, g in itertools.groupby(rows, key=lambda r: r["n"]):
     groups = list(g)
     # take the row with the lowest total time
     min_row = min(groups, key=lambda row: row["total"])
-    min_rows.append(min_row)
-writer = csv.DictWriter(sys.stdout, fieldnames=headers, delimiter="\t")
+    casted = {h: cast(min_row[h]) for h,cast in headers.items()}
+    min_rows.append(casted)
+writer = csv.DictWriter(sys.stdout, fieldnames=headers.keys(), delimiter="\t")
 writer.writeheader()
+min_rows.sort(key=lambda r: r["n"])
 writer.writerows(min_rows)
